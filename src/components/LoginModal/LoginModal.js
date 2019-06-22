fdfa06cf042523d3dba70bf57c868bf7b3367290
Adrian Jaskolski <ajaskolski@getresponse.com>
2019-06-22 16:36:23 +0200

    styles for login

--- src/components/LoginModal/LoginModal.js
+++ src/components/LoginModal/LoginModal.js
@@ -1,6 +1,84 @@
+// import React from "react";
+// import { Formik } from "formik";
+// import * as EmailValidator from "email-validator";
+// import * as Yup from "yup";
+//
+//
+// const LoginModal = (props) => (
+//     <Formik
+//         initialValues={{ email: "", password: "" }}
+//         onSubmit={(values, { setSubmitting }) => {
+//             setTimeout(() => {
+//                 console.log("Logging in", values);
+//                 setSubmitting(false);
+//             }, 500);
+//         }}
+//
+//
+//         validationSchema={Yup.object().shape({
+//             email: Yup.string()
+//                 .email()
+//                 .required("Required"),
+//             password: Yup.string()
+//                 .required("No password provided.")
+//                 .min(8, "Password is too short - should be 8 chars minimum.")
+//                 .matches(/(?=.*[0-9])/, "Password must contain a number.")
+//         })}
+//     >
+//         {props => {
+//             const {
+//                 values,
+//                 touched,
+//                 errors,
+//                 isSubmitting,
+//                 handleChange,
+//                 handleBlur,
+//                 handleSubmit,
+//             } = props;
+//             return (
+//                 <form onSubmit={handleSubmit}>
+//                     <label htmlFor="email">Email</label>
+//                     <input
+//                         name="email"
+//                         type="text"
+//                         placeholder="Enter your email"
+//                         value={values.email}
+//                         onChange={handleChange}
+//                         onBlur={handleBlur}
+//                         className={errors.email && touched.email && "error"}
+//                     />
+//                     {errors.email && touched.email && (
+//                         <div className="input-feedback">{errors.email}</div>
+//                     )}
+//                     <label htmlFor="email">Password</label>
+//                     <input
+//                         name="password"
+//                         type="password"
+//                         placeholder="Enter your password"
+//                         value={values.password}
+//                         onChange={handleChange}
+//                         onBlur={handleBlur}
+//                         className={errors.password && touched.password && "error"}
+//                     />
+//                     {errors.password && touched.password && (
+//                         <div className="input-feedback">{errors.password}</div>
+//                     )}
+//                     <button type="submit" disabled={isSubmitting} onClick={props.tryLogin}>
+//                         Login
+//                     </button>
+//                 </form>
+//             );
+//         }}
+//     </Formik>
+// );
+//
+// export default LoginModal;
+
+
 import React from "react";
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import styles from './LoginModal.module.css'
 
 
 class LoginModal extends React.Component {
@@ -21,7 +99,7 @@ class LoginModal extends React.Component {
         this.setState({
             [event.target.id]: event.target.value
         });
-    }
+    };
 
     handleSubmit = event => {
         event.preventDefault();
@@ -34,10 +112,11 @@ class LoginModal extends React.Component {
         }
 
         return (
-            <div className="Login" style={{margin: '200px'}}>
-                <Form onSubmit={this.handleSubmit}>
+            <div className={styles.Login}>
+                <Form onSubmit={this.handleSubmit} className={styles.Loginform}>
                     <Form.Group controlId="email">
                         <Form.Control
+                            placeholder="Email"
                             autoFocus
                             type="email"
                             value={this.state.email}
@@ -49,6 +128,7 @@ class LoginModal extends React.Component {
                             value={this.state.password}
                             onChange={this.handleChange}
                             type="password"
+                            placeholder="Password"
                         />
                     </Form.Group>
                     <Button
