a2de7f778ed0b76e31f8f846bed901d4ec7cd515
Adrian Jaskolski <ajaskolski@getresponse.com>
2019-07-11 16:55:38 +0200

    delete unused category

--- src/components/ChartBar/ChartBar.js
+++ src/components/ChartBar/ChartBar.js
@@ -1,25 +1,23 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Bar} from 'react-chartjs-2'
 
-class ChartBar extends Component{
-    constructor(propsBar){
+class ChartBar extends Component {
+    constructor(propsBar) {
         super(propsBar);
         this.state = {
-            chartDataLine:{
-                labels:['Math', 'Biology', 'Physics', 'Chemistry', 'History'],
-                datasets:[
+            chartDataLine: {
+                labels: ['Math', 'Biology', 'Physics', 'Chemistry'],
+                datasets: [
                     {
-                        label:'Tests Count',
-                        data:[
+                        label: 'Tests Count',
+                        data: [
                             26,
                             58,
                             33,
                             12,
-                            41
                         ],
-                        backgroundColor:[
+                        backgroundColor: [
                             'rgb(0, 123, 255)',
-                            '#6c757d',
                             '#dc3545',
                             '#ffc107',
                             '#28a745',
@@ -32,35 +30,31 @@ class ChartBar extends Component{
         }
     }
 
-    static defaultProps ={
+    static defaultProps = {
         displayTitle: true,
         displayLegend: true,
         legendPosition: 'right'
     };
 
-    render(){
-        return(
-<div className ="chartBar">
-    <Bar
+    render() {
+        return (
+            <div className="chartBar">
+                <Bar
 
-        data={this.state.chartDataLine}
-        width={400}
-        height={400}
-        options={{
-            maintainAspectRatio: false,
+                    data={this.state.chartDataLine}
+                    width={400}
+                    height={400}
+                    options={{
+                        maintainAspectRatio: false,
 
-            title:{
-                display: this.props.displayTitle,
-                text: 'Question By Categories',
-                fontSize: 25
-            },
-            // legend:{
-            //     display: this.props.displayLegend,
-            //     position: this.props.legendPosition
-            // }
-        }}
-    />
-</div>
+                        title: {
+                            display: this.props.displayTitle,
+                            text: 'Question By Categories',
+                            fontSize: 25
+                        },
+                    }}
+                />
+            </div>
         )
     }
 }
--- src/components/ChartPie/ChartPie.js
+++ src/components/ChartPie/ChartPie.js
@@ -1,28 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import {Pie} from 'react-chartjs-2'
 
-class ChartPie extends Component{
-    constructor(propsPie){
+class ChartPie extends Component {
+    constructor(propsPie) {
         super(propsPie);
         this.state = {
-            chartDataLine:{
-                labels:['Math', 'Biology', 'Physics', 'Chemistry', 'History'],
-                datasets:[
+            chartDataLine: {
+                labels: ['Math', 'Biology', 'Physics', 'Chemistry'],
+                datasets: [
                     {
-                        label:'Question Solved',
-                        data:[
+                        label: 'Question Solved',
+                        data: [
                             26,
                             58,
                             33,
                             12,
-                            41
                         ],
-                        backgroundColor:[
-                         'rgb(0, 123, 255)',
-                         '#6c757d',
-                         '#dc3545',
-                         '#ffc107',
-                         '#28a745',
+                        backgroundColor: [
+                            'rgb(0, 123, 255)',
+                            '#dc3545',
+                            '#ffc107',
+                            '#28a745',
                         ]
 
                     }
@@ -33,45 +31,43 @@ class ChartPie extends Component{
     }
 
 
-
-    static defaultProps ={
+    static defaultProps = {
         displayTitle: true,
         displayLegend: true,
         legendPosition: 'bottom'
     }
 
 
-    render(){
-        return(
-<div className ="chartPie">
-    <Pie
-        data={this.state.chartDataLine}
-        width={400}
-        height={400}
-        labels = {50}
+    render() {
+        return (
+            <div className="chartPie">
+                <Pie
+                    data={this.state.chartDataLine}
+                    width={400}
+                    height={400}
+                    labels={50}
 
 
-        options={{
-                maintainAspectRatio: false,
+                    options={{
+                        maintainAspectRatio: false,
 
-            title:{
-                display: this.props.displayTitle,
-                text: 'Questions Solved:',
-                fontSize: 25,
+                        title: {
+                            display: this.props.displayTitle,
+                            text: 'Questions Solved:',
+                            fontSize: 25,
 
-            },
-            legend:{
-                display: this.props.displayLegend,
-                position: this.props.legendPosition,
+                        },
+                        legend: {
+                            display: this.props.displayLegend,
+                            position: this.props.legendPosition,
 
-            },
+                        },
 
-        }}
-    />
-</div>
+                    }}
+                />
+            </div>
 
         )
-
 
 
     }
