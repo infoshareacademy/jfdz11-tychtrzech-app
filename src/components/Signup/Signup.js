28ee87215cbdcad256d58cb7c3920de9823ad513
Maciej Lewandowski <maciejlew94@gmail.com>
2019-08-11 20:33:50 +0200

    Signup to firebase is working

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,87 +1,90 @@
 import React from "react";
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import firebase from "firebase";
 
 class Signup extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            ...props,
-            email: "",
-            password: ""
-        };
-    }
-
-    validateForm() {
-        return this.state.email.length > 0 && this.state.password.length > 0;
-    }
-
-    handleChange = event => {
-        this.setState({
-            [event.target.id]: event.target.value
-        });
+  constructor(props) {
+    super(props);
+    this.state = {
+      ...props,
+      email: "",
+      password: ""
     };
+  }
 
-    handleSubmit = event => {
-        event.preventDefault();
-    };
+  validateForm() {
+    return this.state.email.length > 0 && this.state.password.length > 0;
+  }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.id]: event.target.value
+    });
+  };
 
-    render() {
-        if(this.props.login === true){
-            return null
-        }
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(this.state);
+    firebase
+      .auth()
+      .createUserWithEmailAndPassword(this.state.email, this.state.password);
+    // firebase.auth().createUserWithEmailAndPassword(email, password).catch(function(error) {
+    //     // Handle Errors here.
+    //     var errorCode = error.code;
+    //     var errorMessage = error.message;
+    //     // ...
+    //   });
+  };
 
-        return (
-            <div style={{marginTop: '50px', padding: '20px'}}>
-                <Form onSubmit={this.handleSubmit}>
-                    {/* <Form.Group controlId="name">
-                        <Form.Control
-                            placeholder="Name"
-                            autoFocus
-                            type="text"
-                            value={this.state.name}
-                            onChange={this.handleChange}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="surname">
-                        <Form.Control
-                            placeholder="Surname"
-                            type="email"
-                            value={this.state.surname}
-                            onChange={this.handleChange}
-                        />
-                    </Form.Group> */}
-                    <Form.Group controlId="email">
-                        <Form.Control
-                            placeholder="Email"
-                            type="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                    </Form.Group>
-                    <Form.Group controlId="password">
-                        <Form.Control
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                            type="password"
-                            placeholder="Password"
-                        />
-                    </Form.Group>
-                    <Button
-                        block
-                        // disabled={!this.validateForm()}
-                        type="submit"
-                    >
-                        Signup
-                    </Button>
-                    <p style={{marginTop: '30px', display: 'inline' }}> Have account already?</p>
-                    <Button style={{display: 'inline'}} variant="link"
-                            size={"sm"} href={'/login'}> Login!</Button>
-                </Form>
-            </div>
-        );
+  render() {
+    if (this.props.login === true) {
+      return null;
     }
+
+    return (
+      <div style={{ marginTop: "50px", padding: "20px" }}>
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group controlId="email">
+            <Form.Control
+              placeholder="Email"
+              type="email"
+              value={this.state.email}
+              onChange={this.handleChange}
+            />
+          </Form.Group>
+          <Form.Group controlId="password">
+            <Form.Control
+              value={this.state.password}
+              onChange={this.handleChange}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Group>
+          <Button
+            block
+            // disabled={!this.validateForm()}
+            type="submit"
+          >
+            Signup
+          </Button>
+          <p style={{ marginTop: "30px", display: "inline" }}>
+            {" "}
+            Have account already?
+          </p>
+          <Button
+            style={{ display: "inline" }}
+            variant="link"
+            size={"sm"}
+            href={"/login"}
+          >
+            {" "}
+            Login!
+          </Button>
+        </Form>
+      </div>
+    );
+  }
 }
 
-export default Signup
+export default Signup;
